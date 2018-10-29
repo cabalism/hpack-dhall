351e6c4e79cc188333b1a8890a2da01232a5a4bc
Phil de Joux <philderbeast@gmail.com>
2018-10-29 09:03:00 -0400

    Show help for dhall-hpack-dhall, #15.

--- exe/dhall-hpack-dhall/DhallMain.hs
+++ exe/dhall-hpack-dhall/DhallMain.hs
@@ -1,14 +1,39 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ApplicativeDo #-}
 
 module Main (main) where
 
-import System.Environment (getArgs)
+import Data.Monoid ((<>))
 import Hpack.Dhall (showDhall, packageConfig)
+import Options.Applicative (Parser, ParserInfo)
+import qualified Options.Applicative as O
+
+data Options = Options {file :: String}
+
+parseOptions :: Parser Options
+parseOptions = O.helper <*> do
+    file <- parseFile
+    return (Options {..})
+  where
+    parseFile =
+        O.strOption $
+        O.long "package-dhall"
+        <> O.metavar "FILE"
+        <> O.value packageConfig
+        <> O.showDefault
+        <> O.help "A record of hpack fields"
+
+parserInfo :: ParserInfo Options
+parserInfo =
+    O.info
+        parseOptions $
+        O.fullDesc
+        <> O.header "Hpack as Dhall"
+        <> O.progDesc "Show the compiled dhall expression of an hpack package description file."
 
 main :: IO ()
-main =
-    getArgs
-    >>= \ case
-            [s] -> showDhall s
-            _ -> showDhall packageConfig
-    >>= putStrLn
+main = do
+    Options {..} <- O.execParser parserInfo
+    s <- showDhall file
+    putStrLn s
+    return ()
