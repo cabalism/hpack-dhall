656808b0462537360caaeeec9c505ae04b93ae50
Phil de Joux <philderbeast@gmail.com>
2026-04-17 13:15:42 -0400

    Satisfy fourmolu

--- exe/dhall-hpack-cabal/CabalMain.hs
+++ exe/dhall-hpack-cabal/CabalMain.hs
@@ -1,30 +1,33 @@
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main (main) where
 
-import Paths_hpack_dhall (version)
-import Data.Version (showVersion)
 import Data.Foldable (asum)
-import qualified Options.Applicative as O
-import Options
-    ( parsePkgFile, parseNumericVersion, parseVersion
-    , parseForce, parseQuiet
-    )
-import qualified Hpack as H (hpack, version, getOptions, setDecode)
+import Data.Version (showVersion)
+import qualified Hpack as H (getOptions, hpack, setDecode, version)
 import Hpack.Dhall (fileToJson)
+import Options
+  ( parseForce
+  , parseNumericVersion
+  , parsePkgFile
+  , parseQuiet
+  , parseVersion
+  )
+import qualified Options.Applicative as O
+import Paths_hpack_dhall (version)
 
 data Command = NumericVersion | Version | Run Options
 
-data Options =
-    Options
-        { pkgFile :: String
-        , force :: Bool
-        , quiet :: Bool
-        }
+data Options = Options
+  { pkgFile :: String
+  , force :: Bool
+  , quiet :: Bool
+  }
 
 parseOptions :: O.Parser Options
-parseOptions = O.helper <*> do
+parseOptions =
+  O.helper <*> do
     pkgFile <- parsePkgFile
     force <- parseForce
     quiet <- parseQuiet
@@ -32,37 +35,37 @@ parseOptions = O.helper <*> do
 
 parserInfo :: O.ParserInfo Command
 parserInfo =
-    O.info parser $
-        O.fullDesc
-        <> O.header "Hpack's dhalling"
-        <> O.progDesc "Write the .cabal for a .dhall package description, resolving imports."
-    where
-        parser = asum
-            [ NumericVersion <$ parseNumericVersion
-            , Version <$ parseVersion
-            , Run <$> parseOptions
-            ]
+  O.info parser $
+    O.fullDesc
+      <> O.header "Hpack's dhalling"
+      <> O.progDesc "Write the .cabal for a .dhall package description, resolving imports."
+  where
+    parser =
+      asum
+        [ NumericVersion <$ parseNumericVersion
+        , Version <$ parseVersion
+        , Run <$> parseOptions
+        ]
 
 main :: IO ()
 main = do
-    command <- O.execParser parserInfo
+  command <- O.execParser parserInfo
 
-    case command of
-        NumericVersion ->
-            putStrLn $ showVersion version
-
-        Version -> do
-            putStrLn $ "dhall-hpack-cabal-" ++ showVersion version
-            putStrLn $ "hpack-" ++ showVersion H.version
-
-        Run Options{..} -> do
-            opts <- H.getOptions pkgFile $
-                mconcat
-                    [ [ "--force" | force ]
-                    , [ "--silent" | quiet ]
-                    ]
-            case opts of
-                Just (verbose, options) ->
-                    H.hpack verbose (H.setDecode fileToJson options)
-                Nothing ->
-                    return ()
+  case command of
+    NumericVersion ->
+      putStrLn $ showVersion version
+    Version -> do
+      putStrLn $ "dhall-hpack-cabal-" ++ showVersion version
+      putStrLn $ "hpack-" ++ showVersion H.version
+    Run Options{..} -> do
+      opts <-
+        H.getOptions pkgFile $
+          mconcat
+            [ ["--force" | force]
+            , ["--silent" | quiet]
+            ]
+      case opts of
+        Just (verbose, options) ->
+          H.hpack verbose (H.setDecode fileToJson options)
+        Nothing ->
+          return ()
